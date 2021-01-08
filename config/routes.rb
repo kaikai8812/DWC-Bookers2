c850a0e7211644ea8d5e642585d40a3494fa1066
kaikai8812 <sauna.kaiaki@gmail.com>
2021-01-08 08:11:44 +0000

    chapter1-完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   get 'home/about' => 'homes#about'
+  
   devise_for :users
+  
   root to: 'homes#top'
   resources :users, only: [:show, :index] 
     resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
