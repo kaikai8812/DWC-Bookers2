44d7555004a6bb427b044c6a195aa5cfadfb5dfc
“kaikai8812” <sauna.kaikai@gmail.com>
2021-02-01 13:05:55 +0000

    いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
   end
 end
