2ab233fdad64f322bab1bf70ca478f55bc013c08
“kaikai8812” <sauna.kaikai@gmail.com>
2021-02-02 12:14:06 +0000

    応用課題４完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,16 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def follows
+    @user = User.find(params[:id])
+    @users = @user.followings
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followeds
+  end
 
   private
 
