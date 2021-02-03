fc97de3919c304630f833dadb6357ca0f66f7c4a
“kaikai8812” <sauna.kaikai@gmail.com>
2021-02-03 07:40:16 +0000

    layout少し変更

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def search
-    @model = params["search"]["model"]
+    @model = params["aaa"]["model"]
     @value = params["search"]["value"]
     @how = params["search"]["how"]
     @datas = search_for(@how,@model,@value)
