41b0af23ac9c6b61aa427a5d36d68ad553935329
“kaikai8812” <sauna.kaikai@gmail.com>
2021-02-06 10:21:52 +0000

    Ajax コメントエラー　未達成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,19 +2,26 @@ class BookCommentsController < ApplicationController
   
   def create
     # binding.pry
-   @book = Book.find(params[:book_id])
-   @comment = BookComment.new(book_comment_params)
-   @comment.user_id = current_user.id
-   @comment.book_id = @book.id
-  @comment.save
+     @book = Book.find(params[:book_id])
+     @comment = BookComment.new(book_comment_params)
+     @comment.user_id = current_user.id
+     @comment.book_id = @book.id
+    if @comment.save
+      render 'create'
+    else
+      # binding.pry
+      render 'error'
+    end
+  end
     # render 'comment_index'
   # redirect_to request.referer
-  end
+  
   
   def destroy
   @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
   @comment.destroy
   @book=Book.find(params[:book_id])
+    render 'destroy'
   # render :comment_index
   # redirect_to request.referer
   end
